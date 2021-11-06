c0dabec635f14d3dc2e02dc1687264c6c35551d0
Simo Aleksandrov <simo3003@me.com>
2021-11-06 15:55:49 +0200

    init

---
+++ go.mod
@@ -0,0 +1,11 @@
+module api
+
+// +heroku goVersion go1.17
+go 1.17
+
+require (
+	github.com/go-chi/chi/v5 v5.0.5
+	github.com/go-chi/httplog v0.2.1
+)
+
+require github.com/rs/zerolog v1.18.1-0.20200514152719-663cbb4c8469 // indirect
---
+++ main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/httplog"
+)
+
+func main() {
+	// Logger
+	logger := httplog.NewLogger("httplog-example", httplog.Options{
+		// JSON: true,
+		Concise: true,
+		// Tags: map[string]string{
+		// 	"version": "v1.0-81aa4244d9fc8076a",
+		// 	"env":     "dev",
+		// },
+	})
+
+	// Service
+	r := chi.NewRouter()
+	r.Use(httplog.RequestLogger(logger))
+	r.Use(middleware.Heartbeat("/ping"))
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("hello world"))
+	})
+
+	r.Get("/panic", func(w http.ResponseWriter, r *http.Request) {
+		panic("oh no")
+	})
+
+	r.Get("/info", func(w http.ResponseWriter, r *http.Request) {
+		oplog := httplog.LogEntry(r.Context())
+		w.Header().Add("Content-Type", "text/plain")
+		oplog.Info().Msg("info here")
+		w.Write([]byte("info here"))
+	})
+
+	r.Get("/warn", func(w http.ResponseWriter, r *http.Request) {
+		oplog := httplog.LogEntry(r.Context())
+		oplog.Warn().Msg("warn here")
+		w.WriteHeader(400)
+		w.Write([]byte("warn here"))
+	})
+
+	r.Get("/err", func(w http.ResponseWriter, r *http.Request) {
+		oplog := httplog.LogEntry(r.Context())
+		oplog.Error().Msg("err here")
+		w.WriteHeader(500)
+		w.Write([]byte("err here"))
+	})
+
+	http.ListenAndServe(":80", r)
+}
+
